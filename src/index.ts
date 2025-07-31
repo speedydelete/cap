82a5f010ddfb93eee5e3c8a9ce5b2de1a21612da
speedydelete <speedykorok777@gmail.com>
2025-07-31 10:02:20 -0400

    change cli 2

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 
 import {tokenize} from './tokenizer.js';
-import {replaceVariables} from './variables.js';
+import {expand} from './expand.js';
 import {Pattern} from './pattern.js';
 import {console, disableLoggingToConsole} from './apis.js';
 
 export * as tokenizer from './tokenizer.js';
 export {clearFileCache} from './tokenizer.js';
-export * as variables from './variables.js';
+export * as variables from './expand.js';
 export * as rle from './pattern.js';
 export {getStdout, getStderr, clearStdout, clearStderr} from './apis.js';
 
@@ -15,7 +15,7 @@ export interface CompilerOptions {
     argv?: string[];
     allowJSImports?: boolean;
     debugTokens?: boolean;
-    debugReplaced?: boolean;
+    debugExpanded?: boolean;
     dontLogToConsole?: boolean;
 }
 
@@ -27,8 +27,8 @@ export async function compile(filePath: string, options: CompilerOptions = {}): 
     if (options.debugTokens) {
         console.log(tokens.map(x => x.value).join(' ').replaceAll('\n ', '\n'));
     }
-    tokens = replaceVariables(tokens);
-    if (options.debugReplaced) {
+    tokens = expand(tokens);
+    if (options.debugExpanded) {
         console.log(tokens.map(x => x.value).join(' ').replaceAll('\n ', '\n'));
     }
     return Pattern.fromTokens(tokens).toRLE();
